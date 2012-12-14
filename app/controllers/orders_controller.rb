d502d01429933f7f9e8712af20cd4c29c770a9e5
Elizabeth Rubenfield <lrubenfield@gmail.com>
2012-12-14 17:06:41 -0500

    last commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,9 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
+    
     @orders = Order.all
-
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @orders }
